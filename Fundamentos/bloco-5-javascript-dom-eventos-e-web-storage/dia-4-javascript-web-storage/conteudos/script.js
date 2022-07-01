26c1ce145146a6f5f46acf230126d638ce5826ef
iamlucasgomes <lucas.devjs@gmail.com>
2022-07-01 15:47:23 -0300

    UPLOAD: exercicios bloco 5.4 upload dos exemplos de local storage e session storage

---
+++ Fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-4-javascript-web-storage/conteudos/script.js
@@ -0,0 +1,19 @@
+console.log(localStorage.length); // não possui nada salvo, então o retorno é o valor: 0
+localStorage.setItem('firstname', 'Adam'); // salva uma entrada com a key = 'firstname' e value = 'Adam'
+localStorage.setItem('lastname', 'Smith'); // salva uma entrada com a key = 'lastname' e value = 'Smith'
+console.log(localStorage.getItem('lastname')); // retorna o valor 'Smith'
+console.log(localStorage.length); // possui duas entradas, então o retorno é o valor: 2
+localStorage.removeItem('lastname'); // remove a entrada referente a key = 'lastname'
+console.log(localStorage.length); // possui uma entrada, então o retorno é o valor: 1
+localStorage.clear(); // limpa todas as entradas salvas em localStorage
+console.log(localStorage.length); // não possui nada salvo, então o retorno é o valor: 0
+
+console.log(sessionStorage.length); // não possui nada salvo, então o retorno é o valor: 0
+sessionStorage.setItem('firstname', 'Adam'); // salva uma entrada com a key = 'firstname' e value = 'Adam'
+sessionStorage.setItem('lastname', 'Smith'); // salva uma entrada com a key = 'lastname' e value = 'Smith'
+console.log(sessionStorage.getItem('lastname')); // retorna o valor 'Smith'
+console.log(sessionStorage.length); // possui duas entradas, então o retorno é o valor: 2
+sessionStorage.removeItem('lastname'); // remove a entrada referente a key = 'lastname'
+console.log(sessionStorage.length); // possui uma entrada, então o retorno é o valor: 1
+sessionStorage.clear(); // limpa todas as entradas salvas em sessionStorage
+console.log(sessionStorage.length); // não possui nada salvo, então o retorno é o valor: 0
