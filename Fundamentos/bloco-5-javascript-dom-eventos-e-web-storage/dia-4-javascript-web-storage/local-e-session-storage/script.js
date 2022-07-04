236d54bcc0880023b342abbff2c38d831314e30c
iamlucasgomes <lucas.devjs@gmail.com>
2022-07-04 12:35:47 -0300

    UPLOAD: exercicios bloco 5.4

---
+++ Fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-4-javascript-web-storage/local-e-session-storage/script.js
@@ -0,0 +1,13 @@
+const slider = document.querySelector('.slider');
+const footer = document.querySelector('footer');
+
+slider.addEventListener('click', dark);
+
+function dark() {
+  const body = document.querySelector('body');
+  body.classList.toggle('dark');
+}
+
+function font() {
+  document
+}
