f289122bb0b34cc3d43fc22c3caa80da7f7608a5
iamlucasgomes <lucas.devjs@gmail.com>
2022-07-03 21:43:31 -0300

    UPLOAD: exercicios bloco 5.3 upload do calendário completo

--- Fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/calendario/script.js
+++ Fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/calendario/script.js
@@ -48,10 +48,10 @@ function decemberDays() {
 
 function btnHolidays(string) {
   const btnContainer = document.querySelector('.buttons-container');
- const button = document.createElement('button');
- button.setAttribute('id', 'btn-holiday');
- button.innerText = string;
- btnContainer.appendChild(button);
+  const button = document.createElement('button');
+  button.setAttribute('id', 'btn-holiday');
+  button.innerText = string;
+  btnContainer.appendChild(button);
 }
 
 const holidayBtn = document.querySelector('#btn-holiday');
@@ -64,20 +64,20 @@ function holidayBgBlack() {
   const holiday = document.querySelectorAll('.holiday');
   for (const days of holiday) {
     if (days.style.backgroundColor === newColor) {
-     days.style.backgroundColor = backgroundColor;
+      days.style.backgroundColor = backgroundColor;
     } else {
       days.style.backgroundColor = newColor;
     }
-    
-  } 
+
+  }
 }
 
 function btnFriday(string) {
   const btnContainer = document.querySelector('.buttons-container');
- const button = document.createElement('button');
- button.setAttribute('id', 'btn-friday');
- button.innerText = string;
- btnContainer.appendChild(button);
+  const button = document.createElement('button');
+  button.setAttribute('id', 'btn-friday');
+  button.innerText = string;
+  btnContainer.appendChild(button);
 }
 
 const fridayBtn = document.querySelector('#btn-friday');
@@ -86,12 +86,99 @@ fridayBtn.addEventListener('click', textFriday);
 function textFriday() {
 
   const fridays = document.querySelectorAll('.friday');
-  for (const friday of fridays) {
-     friday.innerText = 'SextouBB'
+  const days = [4, 11, 18, 25]
+  const sextou = 'sextouBB'
+  for (let index = 0; index < fridays.length; index += 1) {
+    if (fridays[index].innerText !== sextou) {
+      fridays[index].innerText = sextou;
+    } else {
+      fridays[index].innerText = days[index];
     }
-  } 
+  }
+}
+const days = document.querySelectorAll('.day');
+for (const day of days) {
+  day.addEventListener('mouseenter', zoomIn);
+  day.addEventListener('mouseleave', zoomOut);
+  day.addEventListener('click', setColor);
+}
+
+function zoomIn(event) {
+  event.target.style.fontSize = '30px'
+}
+
+function zoomOut(event) {
+  event.target.style.fontSize = '20px'
+}
+
+function task(string) {
+  const myTasks = document.querySelector('.my-tasks');
+  const span = document.createElement('span');
+  span.innerText = string
+ myTasks.appendChild(span)
+}
+task('Projeto:');
+
+function subtitle(cor) {
+  const div = document.createElement('div');
+  div.classList.add('task');
+  div.style.backgroundColor = cor;
+  const myTasks = document.querySelector('.my-tasks');
+  myTasks.appendChild(div);
+}
+
+subtitle('rgb(220, 20, 60)')
+
+const tasks = document.querySelectorAll('.task');
+for (const task of tasks) {
+  task.addEventListener('click', select);
+}
+
+let taskAtual = tasks;
+
+function select() {
+  const tasks = document.querySelector('.task');
+  tasks.classList.toggle('selected')
+}
 
 
-// 🚀 Exercício 5:
-// Implemente uma função que modifica o texto exibido nos dias que são Sexta-feira. Adicione ao botão "Sexta-feira" um evento de "click" e modifique o texto a ser exibido nos dias que são sextas-feiras.
-// 👀 É interessante que esse botão possua também a lógica inversa. Ao ser clicado novamente, ele retorna à configuração inicial exibindo os dias.
+function setColor(event) {
+  let contain = taskAtual[0].classList.contains('selected');
+  const select = document.querySelector('.task').style.backgroundColor;
+  const initialColor = 'rgb(119,119,119)';
+  if(contain){
+  if(event.target.style.color !== select){
+    event.target.style.color = select;
+  }else {
+    event.target.style.color = initialColor;
+  };
+}
+}
+
+const input = document.querySelector('#task-input');
+const btnAdd = document.querySelector('#btn-add');
+
+btnAdd.addEventListener('click', commitments);
+input.addEventListener('keyup', function (enter) {
+  key =  enter.keyCode;
+  if (key === 13) {
+    commitments();
+  }
+})
+
+function commitments() {
+  const taskList = document.querySelector('.task-list');
+  const paragraph = document.createElement('p');
+  if(input.value.length === 0){
+    alert('Ops, você deve adicionar um compromisso!')
+    return;
+  }
+  paragraph.innerText = input.value;
+  taskList.appendChild(paragraph);
+  input.value = '';
+}
+// 🚀 Bônus:
+// Vamos adicionar compromissos ao seu calendário? Implemente uma função que, ao digitar um compromisso na caixa de texto "COMPROMISSOS", adiciona o item à lista "MEUS COMPROMISSOS" ao clicar no botão "ADICIONAR".
+// Se nenhum caractere for inserido no campo input, a função deve retornar um alert com uma mensagem de erro ao clicar em "ADICIONAR".
+// Ao pressionar a tecla "enter" o evento também deverá ser disparado.
+// Dica - Propriedade: key.
