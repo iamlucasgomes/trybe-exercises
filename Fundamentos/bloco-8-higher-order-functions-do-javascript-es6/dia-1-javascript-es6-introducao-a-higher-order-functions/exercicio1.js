5a9d246417ff2bdb70201b8ab016e84579329559
iamlucasgomes <lucas.devjs@gmail.com>
2022-07-21 22:11:36 -0300

    UPDATE: exercicio2 OK

--- Fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/exercicio1.js
+++ Fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/exercicio1.js
@@ -1,6 +1,6 @@
 // 1 - Crie uma função que retorne um objeto no formato { nomeCompleto, email } representando uma nova pessoa contratada. Passe sua função como parâmetro da HOF newEmployees para criar cada pessoa contratada em seu respectivo id. A sua função deve receber como parâmetro o nome completo da pessoa funcionária e a partir dele gerar automaticamente um email no formato nome_da_pessoa@trybe.com.
 
-const PersonData = (person) => ({NomeCompleto:person, email: `${person.replace(/ /, '_')}@trybe.com`});
+const PersonData = (person) => ({NomeCompleto:person, email: `${person.toLowerCase().replace(/ /, '_')}@trybe.com`});
 
 const newEmployees = () => {
   
