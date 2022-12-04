140f736fe55e93700b31337a91803282e5d8407e
iamlucasgomes <lucas.devjs@gmail.com>
2022-12-04 20:30:47 -0300

    Chore: para fixar sobre LEFT,RIGHT e SELF JOIN

---
+++ Back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-2-descomplicando-joins/leftAndRightJoin.sql
@@ -0,0 +1,43 @@
+-- Active: 1668632984497@@localhost@3306@sakila
+
+-- LEFT JOIN
+
+SELECT
+    c.customer_id,
+    c.first_name,
+    c.last_name,
+    a.actor_id,
+    a.first_name,
+    a.last_name
+FROM sakila.customer AS c
+LEFT JOIN sakila.actor AS a
+ON c.last_name = a.last_name
+ORDER BY c.last_name;
+
+-- RIGHT JOIN
+
+SELECT
+    c.customer_id,
+    c.first_name,
+    c.last_name,
+    a.actor_id,
+    a.first_name,
+    a.last_name
+FROM sakila.customer AS c
+RIGHT JOIN sakila.actor AS a
+ON c.last_name = a.last_name
+ORDER BY c.last_name;
+
+-- INNER JOIN
+
+SELECT
+    c.customer_id,
+    c.first_name,
+    c.last_name,
+    a.actor_id,
+    a.first_name,
+    a.last_name
+FROM sakila.customer AS c
+INNER JOIN sakila.actor AS a
+ON c.last_name = a.last_name
+ORDER BY c.last_name;
---
+++ Back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-2-descomplicando-joins/selfJoin.sql
@@ -0,0 +1,11 @@
+-- Active: 1668632984497@@localhost@3306@sakila
+
+-- SElF JOIN
+
+SELECT
+    CONCAT(Employee.first_name, " ", Employee.last_name) AS "Nome da Pessoa Colaboradora",
+    CONCAT(Manager.first_name, " ", Manager.last_name) AS "Nome Gerente"
+FROM
+    employees AS Employee
+INNER JOIN
+    employees AS Manager ON Employee.manager_id = Manager.employee_id;
