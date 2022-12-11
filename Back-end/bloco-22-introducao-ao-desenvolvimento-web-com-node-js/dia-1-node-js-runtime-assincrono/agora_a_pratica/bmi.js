b66e6429e85db050d9472d7d428bc758b686770d
iamlucasgomes <lucas.devjs@gmail.com>
2022-12-11 19:06:21 -0300

    Feat: alterei o input para aceitar valores float

--- Back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-1-node-js-runtime-assincrono/agora_a_pratica/bmi.js
+++ Back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-1-node-js-runtime-assincrono/agora_a_pratica/bmi.js
@@ -4,13 +4,14 @@ const readline = require('readline-sync');
 🚀 Crie um código para calcular o índice de massa corporal(IMC) de uma pessoa. -OK
 🚀 Agora, permita que o código seja executado através do comando npm run bmi. -OK
 🚀 Chegou a hora de tornar nosso código mais interativo! Vamos adicionar inputs de entrada para quem usar. -OK
+🚀 Agora temos um problema, o peso não é um número inteiro! Isso quer dizer que precisamos mudar um pouco a forma como solicitamos o input desse dado.
 🚀 Vamos sofisticar um pouco mais nosso exercício. Além de imprimir o IMC na tela, imprima também em qual categoria da tabela abaixo aquele IMC se enquadra:
 🚀 Realize o download deste arquivo e salve-o como simpsons.json. Utilize o arquivo baixado para realizar os requisitos abaixo.
 */
 
 const BMI = () => {
-  const weight = readline.question('What’ your weight?');
-  const height = readline.question('What’ your height?');
+  const weight = readline.questionInt('What’ your weight?');
+  const height = readline.questionFloat('What’ your height?');
   
   console.log((weight / height ** 2).toFixed(2));
 };
