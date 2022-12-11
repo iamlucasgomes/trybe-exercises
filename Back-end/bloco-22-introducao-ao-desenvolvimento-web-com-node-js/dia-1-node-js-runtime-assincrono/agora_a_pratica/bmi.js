9bdf0e0516381ae78a2864b9a09f1937fd589ac5
iamlucasgomes <lucas.devjs@gmail.com>
2022-12-11 18:59:44 -0300

    Feat: Edite o código para que os valores de weight e height sejam informados pela pessoa ao responder as perguntas: “What’ your weight?” e “What’ your height?”, deve-se utilizar o pacote readline-sync.

--- Back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-1-node-js-runtime-assincrono/agora_a_pratica/bmi.js
+++ Back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-1-node-js-runtime-assincrono/agora_a_pratica/bmi.js
@@ -1,11 +1,18 @@
+const readline = require('readline-sync');
+
 /* 
 🚀 Crie um código para calcular o índice de massa corporal(IMC) de uma pessoa. -OK
 🚀 Agora, permita que o código seja executado através do comando npm run bmi. -OK
-🚀 Chegou a hora de tornar nosso código mais interativo! Vamos adicionar inputs de entrada para quem usar.
+🚀 Chegou a hora de tornar nosso código mais interativo! Vamos adicionar inputs de entrada para quem usar. -OK
 🚀 Vamos sofisticar um pouco mais nosso exercício. Além de imprimir o IMC na tela, imprima também em qual categoria da tabela abaixo aquele IMC se enquadra:
 🚀 Realize o download deste arquivo e salve-o como simpsons.json. Utilize o arquivo baixado para realizar os requisitos abaixo.
 */
 
-const BMI = (weight, height) => console.log((weight / height ** 2).toFixed(2));
+const BMI = () => {
+  const weight = readline.question('What’ your weight?');
+  const height = readline.question('What’ your height?');
+  
+  console.log((weight / height ** 2).toFixed(2));
+};
 
-BMI(104, 1.83);
+BMI();
