41d707b809714a25bdbdb4651c015cc593ae530b
iamlucasgomes <lucas.devjs@gmail.com>
2022-12-12 00:10:49 -0300

    Feat: C - Crie uma função que altere o arquivo simpsons.json retirando os personagens com id 10 e 6.

--- Back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-1-node-js-runtime-assincrono/agora_a_pratica/simpsons.js
+++ Back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-1-node-js-runtime-assincrono/agora_a_pratica/simpsons.js
@@ -1,11 +1,13 @@
 const fs = require('fs').promises;
 const path = require('path');
 
+const diretorio = './src/assets/simpsons.json';
+
 // A - Crie uma função que leia todos os dados do arquivo e imprima cada personagem no formato id - Nome. Por exemplo: 1 - Homer Simpson.
 
 const charactersList = async () => {
   try {
-    const data = await fs.readFile(path.resolve(__dirname, './src/assets/simpsons.json'));
+    const data = await fs.readFile(path.resolve(__dirname, diretorio));
     const result = JSON.parse(data);
     result.map((simpson) => 
     console.log(`${Object.values(simpson)[0]} - ${Object.values(simpson)[1]}`));
@@ -19,7 +21,7 @@ charactersList();
 // B - Crie uma função que receba o id de uma personagem como parâmetro e retorne uma Promise que é resolvida com os dados da personagem que possui o id informado. Caso não haja uma personagem com o id informado, rejeite a Promise com o motivo “id não encontrado”.
 
 const requestJson = async (id) => {
-  const data = await fs.readFile(path.resolve(__dirname, './src/assets/simpsons.json'));
+  const data = await fs.readFile(path.resolve(__dirname, diretorio));
   const result = JSON.parse(data);
 
   const findId = result.find((character) => Number(character.id) === id);
@@ -36,3 +38,20 @@ const promise = new Promise((resolve, reject) => {
 };
 
 requestJson(1);
+
+// C - Crie uma função que altere o arquivo simpsons.json retirando os personagens com id 10 e 6.
+
+const editJson = async () => {
+  const data = await fs.readFile(path.resolve(__dirname, diretorio));
+  const result = JSON.parse(data);
+
+  try {
+    await fs.writeFile(path.resolve(__dirname, diretorio), JSON.stringify(result
+    .filter((simpson) => Number(simpson.id) !== 6 && Number(simpson.id) !== 10)));
+    console.log('Arquivo escrito com sucesso!');
+  } catch (err) {
+    console.error(`Erro ao escrever o arquivo: ${err.message}`);
+  }
+};
+
+editJson();
