336ddbe949e87fe36682e7d051a5d16249dedf75
iamlucasgomes <lucas.devjs@gmail.com>
2022-12-12 00:26:41 -0300

    Feat: E - Crie uma função que adicione ao arquivo simpsonFamily.json o personagem Nelson Muntz.

--- Back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-1-node-js-runtime-assincrono/agora_a_pratica/simpsons.js
+++ Back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-1-node-js-runtime-assincrono/agora_a_pratica/simpsons.js
@@ -16,8 +16,6 @@ const charactersList = async () => {
   }
 };
 
-charactersList();
-
 // B - Crie uma função que receba o id de uma personagem como parâmetro e retorne uma Promise que é resolvida com os dados da personagem que possui o id informado. Caso não haja uma personagem com o id informado, rejeite a Promise com o motivo “id não encontrado”.
 
 const requestJson = async (id) => {
@@ -37,8 +35,6 @@ const promise = new Promise((resolve, reject) => {
 });
 };
 
-requestJson(1);
-
 // C - Crie uma função que altere o arquivo simpsons.json retirando os personagens com id 10 e 6.
 
 const editJson = async () => {
@@ -53,8 +49,6 @@ const editJson = async () => {
     console.error(`Erro ao escrever o arquivo: ${err.message}`);
   }
 };
-
-editJson();
 
 // D - Crie uma função que leia o arquivo simpsons.json e crie um novo arquivo, chamado simpsonFamily.json, contendo as personagens com id de 1 a 4.
 
@@ -72,4 +66,16 @@ const simpsonFamily = async () => {
   }
 };
 
-simpsonFamily();
+const addNelson = async () => {
+  const data = await fs.readFile(path.resolve(__dirname, './src/assets/simpsonFamily.json'));
+  const result = JSON.parse(data);
+
+  try {
+    await fs.writeFile(path
+      .resolve(__dirname, './src/assets/simpsonFamily.json'), JSON
+      .stringify([...result, { id: String(result.length + 1), name: 'Nelson Muntz' }]));
+    console.log('Arquivo escrito com sucesso!');
+  } catch (err) {
+    console.error(`Erro ao escrever o arquivo: ${err.message}`);
+  }
+};
