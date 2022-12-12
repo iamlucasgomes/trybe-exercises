e44ef37b73ac895e40754ae02d62cc9174bb925d
iamlucasgomes <lucas.devjs@gmail.com>
2022-12-12 11:52:49 -0300

    Feat: validações adicionadas

--- Back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-1-node-js-runtime-assincrono/agora_a_pratica/fibonacci.js
+++ Back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-1-node-js-runtime-assincrono/agora_a_pratica/fibonacci.js
@@ -7,12 +7,22 @@ A sequência de Fibonacci começa com 0 e 1 e os números seguintes são sempre 
 Armazene o código no arquivo fibonacci.js. -OK
 Utilize o readline-sync para realizar o input de dados. -OK
 O código deve ser acionado através do comando npm run fibonacci. -OK
-Não imprima o valor 0, uma vez que ele não está incluso na sequência.
-Quando n = 10, exatamente 10 elementos devem ser exibidos.
-Adicione validações para garantir que o valor informado é um inteiro maior que 0. */
+Não imprima o valor 0, uma vez que ele não está incluso na sequência. -OK
+Quando n = 10, exatamente 10 elementos devem ser exibidos. -OK
+Adicione validações para garantir que o valor informado é um inteiro maior que 0. -OK */
 
 const fibonacciSequence = () => {
-  const n = readline.question('Qual o limite de numeros?');
+  const n = readline.questionFloat('Qual o limite de numeros?');
+
+  if (n <= 0) {
+    console.error('Insira um numero válido maior que 0');
+    return;
+  }
+  if (!Number.isInteger(n)) {
+    console.error('Insira um numero inteiro');
+    return;
+  }
+
   const result = [1, 1];
   for (let index = 0; index < n - 2; index += 1) {
     result.push(result.at(-2) + result.at(-1));
