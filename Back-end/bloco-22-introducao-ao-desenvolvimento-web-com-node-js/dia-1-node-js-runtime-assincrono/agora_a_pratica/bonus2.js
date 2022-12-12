51ca31a3a2924604f949157a03afcddc4477b565
iamlucasgomes <lucas.devjs@gmail.com>
2022-12-12 15:14:48 -0300

    Auto-save

---
+++ Back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-1-node-js-runtime-assincrono/agora_a_pratica/bonus2.js
@@ -0,0 +1,16 @@
+const readline = require('readline-sync');
+
+/* 
+Crie uma função que recebe três parâmetros e retorna uma Promise.
+
+Caso algum dos parâmetros recebidos não seja um número, rejeite a Promise com o motivo "Informe apenas números".
+Caso todos os parâmetros sejam numéricos, some os dois primeiros e multiplique o resultado pelo terceiro ((a + b) * c).
+Caso o resultado seja menor que 50, rejeite a Promise com o motivo "Valor muito baixo"
+Caso o resultado seja maior que 50, resolva a Promise com o valor obtido. 
+*/
+
+const bonus = async (n1, n2, n3) => {
+
+};
+
+bonus(1, 1, 3);
