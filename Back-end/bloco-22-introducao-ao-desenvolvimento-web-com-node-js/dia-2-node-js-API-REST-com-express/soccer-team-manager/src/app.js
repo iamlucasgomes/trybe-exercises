48d4618b474439a5e3bc70129fdf5e8bc2f2d83b
iamlucasgomes <lucas.devjs@gmail.com>
2022-12-14 22:54:45 -0300

    chore: criei o array com os dados dos times no arquivo app.js

--- Back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-2-node-js-API-REST-com-express/soccer-team-manager/src/app.js
+++ Back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-2-node-js-API-REST-com-express/soccer-team-manager/src/app.js
@@ -1,6 +1,21 @@
 // src/app.js
 const express = require('express');
 
+const teams = [
+  {
+    id: 1,
+    name: 'São Paulo Futebol Clube',
+    initials: 'SPF',
+  },
+  {
+    id: 2,
+    name: 'Clube Atlético Mineiro',
+    initials: 'CAM',
+  },
+];
+
 const app = express();
+
+app.get('/', (req, res) => res.status(200).json({ message: 'Olá Mundo!' }));
 
 module.exports = app;
