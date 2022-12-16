c895534d631e20e2aa93965f8283e3a676efbd27
iamlucasgomes <lucas.devjs@gmail.com>
2022-12-16 12:09:58 -0300

    feat: endpoint do tipo put implementado

--- Back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-2-node-js-API-REST-com-express/agora-a-pratica/src/app.js
+++ Back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-2-node-js-API-REST-com-express/agora-a-pratica/src/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const movies = require('./movies.json')
 const writeJson = require('./utils/writeJson')
+const fs = require('fs').promises;
+const path = require('path')
+
 
 const app = express();
 app.use(express.json())
@@ -20,12 +23,18 @@ app.post('/movies', (req, res) => {
   res.status(200).json(newArray)
 })
 
-app.put('/movies/:id', (req, res) => {
-  const { id } = req.params;
-  const newArray = movies.filter((movie) => movie.id === Number(id)).splice().concat({id,...req.body});
-  console.log({...movies, newArray})
-  // writeJson(newArray)
-  res.status(200).json(newArray);
+app.put('/movies/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { movie, price } = req.body;
+    const index = movies.findIndex((element) => element.id === Number(id));
+    movies[index] = { id: Number(id), movie, price };
+    const updatedMovies = JSON.stringify(movies, null, 2);
+    await fs.writeFile(path.resolve(__dirname, './movies.json'), updatedMovies);
+    res.status(200).json(movies[index]);
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
 })
 
 app.delete('/movies/:id', (req, res) => {
