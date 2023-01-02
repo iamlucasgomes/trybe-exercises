a0fb2658201af57931eae51f9915bab41e021c58
iamlucasgomes <lucas.devjs@gmail.com>
2023-01-02 20:09:26 -0300

    chore: middleware de validação do campo price criado

--- Back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-4-node-js-express-e-middlewares/agora-a-pratica/ecoturismo/src/routers/activities.router.js
+++ Back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-4-node-js-express-e-middlewares/agora-a-pratica/ecoturismo/src/routers/activities.router.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const { validateIfNameExists,
   validateNameSize } = require('../middlewares/validateName.middleware');
+const { validateIfPriceExists,
+  validatePriceGreaterThanOrEqualToZero } = require('../middlewares/validatePrice.middleware');
 
 const router = express.Router();
 
-router.post('/activities', validateIfNameExists, validateNameSize, (_req, res) => res.status(201)
-.json({ message: 'Atividade cadastrada com sucesso!' }));
+router.post(
+'/activities', 
+validateIfNameExists,
+validateNameSize, 
+validateIfPriceExists, 
+validatePriceGreaterThanOrEqualToZero, 
+(_req, res) => res.status(201)
+.json({ message: 'Atividade cadastrada com sucesso!' }),
+);
 
 module.exports = router;
