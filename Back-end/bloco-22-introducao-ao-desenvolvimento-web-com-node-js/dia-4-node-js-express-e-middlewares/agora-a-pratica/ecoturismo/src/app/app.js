7c1e1dfa82732101b0743aaa06cb538440a2f35d
iamlucasgomes <lucas.devjs@gmail.com>
2023-01-03 12:46:30 -0300

    exercicio concluído

--- Back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-4-node-js-express-e-middlewares/agora-a-pratica/ecoturismo/src/app/app.js
+++ Back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-4-node-js-express-e-middlewares/agora-a-pratica/ecoturismo/src/app/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const activitiesRouter = require('../routers/activities.router');
+const signupRouter = require('../routers/signup.router');
 
 const app = express();
 
 app.use(express.json());
 app.use(activitiesRouter);
+app.use(signupRouter);
 
 module.exports = app;
