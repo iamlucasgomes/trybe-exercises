31dc87fd0cafff5b3f4d042a413348dbaa74efd5
iamlucasgomes <lucas.devjs@gmail.com>
2023-02-13 18:46:25 -0300

    dia 3

---
+++ Back-end/bloco-25-introducao-a-typescript/dia-3-express-com-typescript/express-typescript/dist/statusCodes.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// ./statusCodes.ts
+const statusCodes = {
+    OK: 200,
+    NOT_FOUND: 404,
+    CREATED: 201,
+    BAD_REQUEST: 400,
+    UNAUTHORIZED: 401,
+    NO_CONTENT: 204,
+};
+exports.default = statusCodes;
