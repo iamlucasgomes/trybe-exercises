f01459f818178c9ef2f480de71af95cd9397d506
iamlucasgomes <lucas.devjs@gmail.com>
2023-04-25 16:09:53 -0300

    feat: escrevendo os primeiros arquivos em python

---
+++ Ciencia-da-computacao/bloco-32-introducao-a-python/dia-1-aprendendo-python/tempCodeRunnerFile.py
@@ -0,0 +1,16 @@
+PI = 3.14  # PI é uma "constante" em nosso módulo
+
+
+def square(side):
+    """Calculate area of square."""
+    return side * side
+
+
+def rectangle(length, width):
+    """Calculate area of rectangle."""
+    return length * width
+
+
+def circle(radius):
+    """Calculate area of circle."""
+    return PI * radius * radius
