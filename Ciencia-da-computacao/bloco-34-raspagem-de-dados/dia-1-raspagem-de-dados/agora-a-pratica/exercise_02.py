0be08135be49e44047e55ffea9020cfce231f35e
iamlucasgomes <lucas.devjs@gmail.com>
2023-05-16 00:42:06 -0300

    feat: exercise 03

--- Ciencia-da-computacao/bloco-34-raspagem-de-dados/dia-1-raspagem-de-dados/agora-a-pratica/exercise_02.py
+++ Ciencia-da-computacao/bloco-34-raspagem-de-dados/dia-1-raspagem-de-dados/agora-a-pratica/exercise_02.py
@@ -1,5 +1,7 @@
 # Exercício 2:
-# Faça uma requisição ao recurso usuários da API do Github (https://api.github.com/users), exibindo o username e url de todos os usuários retornados.
+# Faça uma requisição ao recurso usuários da
+# API do Github (https://api.github.com/users),
+# exibindo o username e url de todos os usuários retornados.
 
 from requests import get
 
